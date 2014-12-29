bf4f14dbfd43230186f7c9ea2a0161c019cd73a5
François Pluchino <francois.pluchino@sonatra.com>
2014-12-29 11:39:26 +0100

    Replace toggle selector

--- js/panel-collapse.js
+++ js/panel-collapse.js
@@ -49,7 +49,7 @@
         this.$element   = $(element);
         this.$toggle    = $(this.options.collapseSelector, this.$element);
 
-        this.$element.on('click.st.panelcollapse', this.options.collapseSelector, this, onToggleAction);
+        this.$element.on('click.st.panelcollapse', this.$toggle.selector, this, onToggleAction);
     },
         old;
 
@@ -96,7 +96,7 @@
      * @this PanelCollapse
      */
     PanelCollapse.prototype.destroy = function () {
-        this.$element.off('click.st.panelcollapse', this.options.collapseSelector, onToggleAction);
+        this.$element.off('click.st.panelcollapse', this.$toggle.selector, onToggleAction);
         this.$element.removeData('st.panelcollapse');
     };
 
